a9feb1eea7120f64653bec6a05cb538b0baf43e0
Brooks, Keanu Lee <Keanu.B.Lee@gatech.edu>
2016-09-19 14:32:30 -0400

    Ui refactoring
    Hopefully it works everywhere now.

---
+++ GTMaps Android/app/src/main/java/edu/gatech/gtmaps/DirectionsActivity.java
@@ -0,0 +1,26 @@
+package edu.gatech.gtmaps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DirectionsActivity extends AppCompatActivity {
+    public String message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.directions);
+        Intent intent = getIntent();
+
+        message = intent.getStringExtra("room");
+        TextView text = (TextView) findViewById(R.id.room_text);
+        text.setText(message);
+
+
+        ImageView iv = (ImageView)findViewById(R.id.ivfloorplan);
+        iv.setImageResource(R.drawable.ccbfloor);
+    }
+}
---
+++ GTMaps Android/app/src/main/java/edu/gatech/gtmaps/MainActivity.java
@@ -0,0 +1,41 @@
+package edu.gatech.gtmaps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, BUILDINGS);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.Building_Choice);
+        textView.setAdapter(adapter);
+
+        ImageView iv = (ImageView)findViewById(R.id.logoiv);
+        iv.setImageResource(R.drawable.gtmaps);
+    }
+
+    private static final String[] BUILDINGS = new String[] {
+            "College of Computing", "Howey","CULC", "Van Leer"
+    };
+
+    public void findBuilding(View view) {
+        Intent intent = new Intent(this, RoomChooseActivity.class);
+        TextView text = (TextView) findViewById(R.id.Building_Choice);
+        String message = text.getText().toString();
+        intent.putExtra("building", message);
+        startActivity(intent);
+    }
+}
+
