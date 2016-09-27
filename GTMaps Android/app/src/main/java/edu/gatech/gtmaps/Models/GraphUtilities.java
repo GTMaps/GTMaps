96556f43f9622db0ea9b96b3287ce85f623d35bb
Christine Shi <yshi84@gatech.edu>
2016-09-27 17:59:10 -0400

    Some part of the graphic functions added. Can determine if the point is on the right side or left side of a vector. See onTheRight method.

---
+++ GTMaps Android/app/src/main/java/edu/gatech/gtmaps/Models/GraphUtilities.java
@@ -0,0 +1,52 @@
+package edu.gatech.gtmaps.Models;
+
+/**
+ * Created by yingqiongshi on 9/27/16.
+ */
+
+//Useful functions on creating graphs.
+public class GraphUtilities {
+    GraphUtilities() {
+
+    }
+
+    Vec R(Vec V) {
+        return new Vec(-V.y, V.x);
+    };                                                             // V turned right 90 degrees (as seen on screen)
+    Vec R(Vec V, float a) {
+        return W((float)Math.cos(a), V, (float)Math.sin(a), R(V));
+    }                                           // V rotated by angle a in radians
+    float dot(Vec U, Vec V) {
+        return U.x*V.x+U.y*V.y;
+    }                                                     // dot(U,V): U*V (dot product U*V)
+    float det(Vec U, Vec V) {
+        return dot(R(U), V);
+    }
+    Vec W(float s, Vec V) {
+        return V.setTo(s*V.x, s*V.y);
+    }                                                      // sV
+    Vec W(Vec U, Vec V) {
+        return V.setTo(U.x+V.x, U.y+V.y);
+    }                                                   // U+V
+    Vec W(Vec U, float s, Vec V) {
+        return W(U, S(s, V));
+    }                                                   // U+sV
+    Vec W(float u, Vec U, float v, Vec V) {
+        return W(S(u, U), S(v, V));
+    }                                   // uU+vV ( Linear combination)
+    Vec S(float s, Vec V) {
+        return new Vec(s*V.x, s*V.y);
+    };
+
+    //if v is on the right side of u(turns rught at u), return true. If v is on the left side of u, return false.
+    boolean onTheRight(Vec u, Vec v) {
+        return det(u,v) < 0;
+    }
+    //a is the top-right point, b is the down-left point. If p is on the right side of ab, return true. Else, return false.
+    boolean onTheRight(Point p, Point a, Point b) {
+        Vec ab = new Vec(b.x-a.x,b.y-a.y);
+        Vec ap = new Vec(p.x-a.x,p.y-a.y);
+        return onTheRight(ab, ap);
+    }
+
+}
---
+++ GTMaps Android/app/src/main/java/edu/gatech/gtmaps/Models/Point.java
@@ -0,0 +1,131 @@
+package edu.gatech.gtmaps.Models;
+
+/**
+ * Created by yingqiongshi on 9/27/16.
+ */
+
+//Each room will be present a Point to indicate their position
+public class Point {
+    float x, y;
+
+    Point(float px, float py) {
+        x = px;
+        y = py;
+    };
+    Point setTo(float px, float py) {
+        x = px;
+        y = py;
+        return this;
+    };
+    Point setTo(Point P) {
+        x = P.x;
+        y = P.y;
+        return this;
+    };
+//    Point setToMouse() {
+//        x = mouseX;
+//        y = mouseY;
+//        return this;
+//    };
+    Point add(float u, float v) {
+        x += u;
+        y += v;
+        return this;
+    }                       // P.add(u,v): P+=<u,v>
+    Point add(Point P) {
+        x += P.x;
+        y += P.y;
+        return this;
+    };                              // incorrect notation, but useful for computing weighted averages
+    Point add(float s, Point P) {
+        x += s*P.x;
+        y += s*P.y;
+        return this;
+    };               // adds s*P
+    Point add(Vec V) {
+        x += V.x;
+        y += V.y;
+        return this;
+    }                              // P.add(V): P+=V
+    Point add(float s, Vec V) {
+        x += s*V.x;
+        y += s*V.y;
+        return this;
+    }                 // P.add(s,V): P+=sV
+    Point translateTowards(float s, Point P) {
+        x+=s*(P.x-x);
+        y+=s*(P.y-y);
+        return this;
+    };  // transalte by ratio s towards P
+    Point scale(float u, float v) {
+        x*=u;
+        y*=v;
+        return this;
+    };
+    Point scale(float s) {
+        x*=s;
+        y*=s;
+        return this;
+    }                                  // P.scale(s): P*=s
+    Point scale(float s, Point C) {
+        x*=C.x+s*(x-C.x);
+        y*=C.y+s*(y-C.y);
+        return this;
+    }    // P.scale(s,C): scales wrt C: P=L(C,P,s);
+//    Point rotate(float a) {
+//        float dx=x, dy=y, c=cos(a), s=sin(a);
+//        x=c*dx+s*dy;
+//        y=-s*dx+c*dy;
+//        return this;
+//    };     // P.rotate(a): rotate P around origin by angle a in radians
+//    Point rotate(float a, Point G) {
+//        float dx=x-G.x, dy=y-G.y, c=cos(a), s=sin(a);
+//        x=G.x+c*dx+s*dy;
+//        y=G.y-s*dx+c*dy;
+//        return this;
+//    };   // P.rotate(a,G): rotate P around G by angle a in radians
+//    Point rotate(float s, float t, Point G) {
+//        float dx=x-G.x, dy=y-G.y;
+//        dx-=dy*t;
+//        dy+=dx*s;
+//        dx-=dy*t;
+//        x=G.x+dx;
+//        y=G.y+dy;
+//        return this;
+//    };   // fast rotate s=sin(a); t=tan(a/2);
+//    pt moveWithMouse() {
+//        x += mouseX-pmouseX;
+//        y += mouseY-pmouseY;
+//        return this;
+//    };
+    // DRAW , WRITE
+//    pt write() {
+//        print("("+x+","+y+")");
+//        return this;
+//    };  // writes Point coordinates in text window
+//    pt v() {
+//        vertex(x, y);
+//        return this;
+//    };  // used for drawing polygons between beginShape(); and endShape();
+//    pt show(float r) {
+//        ellipse(x, y, 2*r, 2*r);
+//        return this;
+//    }; // shows Point as disk of radius r
+//    pt show() {
+//        show(3);
+//        return this;
+//    }; // shows Point as small dot
+//    pt label(String s, float u, float v) {
+//        fill(black);
+//        text(s, x+u, y+v);
+//        noFill();
+//        return this;
+//    };
+//    pt label(String s, Vec V) {
+//        fill(black);
+//        text(s, x+V.x, y+V.y);
+//        noFill();
+//        return this;
+//    };
+
+}
