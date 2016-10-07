2be957f7c3d330b974c63b17065774eeeb90739f
Veronica LeBlanc <betelgeuseRider@gmail.com>
2016-10-07 13:31:44 -0400

    Initialized Database Schema
    Including PK's, Not Null constraints, and FK's.  Types all left as "TEXT" for now.

--- GTMaps Android/app/src/main/java/edu/gatech/gtmaps/DBHelper.java
+++ GTMaps Android/app/src/main/java/edu/gatech/gtmaps/DBHelper.java
@@ -12,14 +12,36 @@ import java.util.HashMap;
 
     public class DBHelper extends SQLiteOpenHelper {
 
-        public static final String DATABASE_NAME = "MyDBName.db";
-        public static final String CONTACTS_TABLE_NAME = "contacts";
-        public static final String CONTACTS_COLUMN_ID = "id";
-        public static final String CONTACTS_COLUMN_NAME = "name";
-        public static final String CONTACTS_COLUMN_EMAIL = "email";
-        public static final String CONTACTS_COLUMN_STREET = "street";
-        public static final String CONTACTS_COLUMN_CITY = "place";
-        public static final String CONTACTS_COLUMN_PHONE = "phone";
+        public static final String DATABASE_NAME = "GTMapsDB.db";
+        public static final String BUILDINGS_TABLE_NAME = "Buildings";
+        public static final String BUILDINGS_ID = "id";
+        public static final String BUILDINGS_NAME = "name";
+
+        public static final String ROOMS_TABLE_NAME = "Rooms";
+        public static final String ROOMS_BUILDING_ID = "b_uid";
+        public static final String ROOMS_FLOOR_ID = "f_id";
+        public static final String ROOMS_HALLWAY_ID = "h_uid";
+        public static final String ROOMS_HALLWAY_SIDE = "h_side";
+        public static final String ROOMS_ROOM_ID = "r_uid";
+        public static final String ROOMS_ROOM_NAME = "r_name";
+        public static final String ROOMS_CENTER_X = "center_x";
+        public static final String ROOMS_CENTER_Y = "center_y";
+        public static final String ROOMS_DOOR_X = "door_x";
+        public static final String ROOMS_DOOR_Y = "door_y";
+
+        public static final String HALLWAYS_TABLE_NAME = "Hallways";
+        public static final String HALLWAYS_BUILDING_ID = "b_uid";
+        public static final String HALLWAYS_FLOOR_ID = "f_id";
+        public static final String HALLWAYS_HALLWAY_ID = "h_uid";
+        public static final String HALLWAYS_HALLWAY_NAME = "r_name";
+        public static final String HALLWAYS_CENTER_X = "center_x";
+        public static final String HALLWAYS_CENTER_Y = "center_y";
+        public static final String HALLWAYS_LENGTH = "length";
+        public static final String HALLWAYS_WIDTH = "width";
+
+        public static final String JUNCTIONS_TABLE_NAME = "Junctions";
+        public static final String JUNCTIONS_HALLWAY_1 = "h_uid1";
+        public static final String JUNCTIONS_HALLWAY_2 = "h_uid2";
         private HashMap hp;
 
         public DBHelper(Context context)
@@ -29,19 +51,91 @@ import java.util.HashMap;
 
         @Override
         public void onCreate(SQLiteDatabase db) {
+
+            //TODO USE STRING FORMATTING INSTEAD OF A BUNCH OF PLUS SIGNS
+
             // TODO Auto-generated method stub
-            db.execSQL(
-                    "create table contacts " +
-                            "(id integer primary key, name text,phone text,email text, street text,place text)"
-            );
+            String CREATE_BUILDINGS_TABLE =
+                    "CREATE TABLE " + BUILDINGS_TABLE_NAME + "("
+                        + BUILDINGS_ID + " INTEGER PRIMARY KEY NOT NULL,"
+                        + BUILDINGS_NAME + " TEXT"
+                        + ")";
+            db.execSQL(CREATE_BUILDINGS_TABLE);
+
+            String CREATE_ROOMS_TABLE = "CREATE TABLE " + ROOMS_TABLE_NAME + "("
+                    + ROOMS_BUILDING_ID + " TEXT, NOT NULL"
+                    + ROOMS_FLOOR_ID + " TEXT, NOT NULL"
+                    + ROOMS_ROOM_ID + " TEXT, NOT NULL"
+                    + ROOMS_ROOM_NAME + " TEXT,"
+
+                    + ROOMS_HALLWAY_ID + " TEXT, NOT NULL"
+                    + ROOMS_HALLWAY_SIDE + " TEXT, NOT NULL"
+                    + ROOMS_CENTER_X + " TEXT,"
+                    + ROOMS_CENTER_Y + " TEXT,"
+                    + ROOMS_DOOR_X + " TEXT,"
+                    + ROOMS_DOOR_Y + " TEXT" +
+
+                    //Primary Key = B_uid,F_id,R_uid
+                    "PRIMARY KEY(" + ROOMS_BUILDING_ID + ROOMS_FLOOR_ID + ROOMS_ROOM_ID +")" +
+
+                    // Foreign Keys = B_uid, H_uid
+                    "FOREIGN KEY(" + ROOMS_BUILDING_ID + ")"
+                            + "REFERENCES " + BUILDINGS_TABLE_NAME + "(" + BUILDINGS_ID + ")" +
+                    "FOREIGN KEY(" + ROOMS_HALLWAY_ID + ")"
+                            + "REFERENCES " + HALLWAYS_TABLE_NAME + "(" + HALLWAYS_HALLWAY_ID + ")"
+
+                    +")";
+            db.execSQL(CREATE_ROOMS_TABLE);
+
+            String CREATE_HALLWAYS_TABLE = "CREATE TABLE " + HALLWAYS_TABLE_NAME + "("
+                    + HALLWAYS_BUILDING_ID + " TEXT, NOT NULL"
+                    + HALLWAYS_FLOOR_ID + " TEXT, NOT NULL"
+                    + HALLWAYS_HALLWAY_ID + " TEXT, NOT NULL"
+                    + HALLWAYS_HALLWAY_NAME + " TEXT,"
+
+                    + HALLWAYS_CENTER_X + " TEXT,"
+                    + HALLWAYS_CENTER_Y + " TEXT,"
+                    + HALLWAYS_LENGTH+ " TEXT,"
+                    + HALLWAYS_WIDTH+ " TEXT" +
+
+                    //Primary Key = B_uid,F_id,H_uid
+                    "PRIMARY KEY(" + HALLWAYS_BUILDING_ID + " " + HALLWAYS_FLOOR_ID + " " + HALLWAYS_HALLWAY_ID +")" +
+
+                    // Foreign Keys = B_uid
+                    "FOREIGN KEY(" + HALLWAYS_BUILDING_ID + ")"
+                    + "REFERENCES " + BUILDINGS_TABLE_NAME + "(" + BUILDINGS_ID + ")"
+
+                    +")";
+            db.execSQL(CREATE_HALLWAYS_TABLE);
+
+            String CREATE_JUNCTIONS_TABLE = "CREATE TABLE " + JUNCTIONS_TABLE_NAME + "("
+                    + JUNCTIONS_HALLWAY_1 + " INTEGER PRIMARY KEY,"
+                    + JUNCTIONS_HALLWAY_2 + " TEXT," +
+
+                    //Primary Key = H_uid1, H_uid2
+                    "PRIMARY KEY(" + JUNCTIONS_HALLWAY_1 + " " + JUNCTIONS_HALLWAY_2+")" +
+
+                    // Foreign Keys = B_uid
+                    "FOREIGN KEY(" + HALLWAYS_BUILDING_ID + ")"
+                    + "REFERENCES " + BUILDINGS_TABLE_NAME + "(" + BUILDINGS_ID + ")"
+
+                    + ")";
+
+            db.execSQL(CREATE_JUNCTIONS_TABLE);
         }
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             // TODO Auto-generated method stub
-            db.execSQL("DROP TABLE IF EXISTS contacts");
+            db.execSQL("DROP TABLE IF EXISTS Buildings");
+            db.execSQL("DROP TABLE IF EXISTS Rooms");
+            db.execSQL("DROP TABLE IF EXISTS Hallways");
+            db.execSQL("DROP TABLE IF EXISTS Junctions");
+
             onCreate(db);
         }
+
+        // #################### EVERYTHING BELOW THIS IS EXAMPLE CODE #######################################
 
         public boolean insertContact  (String name, String phone, String email, String street,String place)
         {
@@ -64,7 +158,7 @@ import java.util.HashMap;
 
         public int numberOfRows(){
             SQLiteDatabase db = this.getReadableDatabase();
-            int numRows = (int) DatabaseUtils.queryNumEntries(db, CONTACTS_TABLE_NAME);
+            int numRows = (int) DatabaseUtils.queryNumEntries(db, ROOMS_TABLE_NAME);
             return numRows;
         }
 
@@ -99,7 +193,7 @@ import java.util.HashMap;
             res.moveToFirst();
 
             while(res.isAfterLast() == false){
-                array_list.add(res.getString(res.getColumnIndex(CONTACTS_COLUMN_NAME)));
+                array_list.add(res.getString(res.getColumnIndex(ROOMS_ROOM_ID)));
                 res.moveToNext();
             }
             return array_list;
