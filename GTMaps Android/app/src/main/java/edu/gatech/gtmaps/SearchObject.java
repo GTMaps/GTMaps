381f2dd6cd183e24e2dceb6a7e4637d7920ba032
timtrent <timtrent@users.noreply.github.com>
2016-10-10 22:33:12 -0400

    Added personal Node implementation
    Added my own quick & dirty Node for graph search, private inner class in
    SearchObject

--- GTMaps Android/app/src/main/java/edu/gatech/gtmaps/SearchObject.java
+++ GTMaps Android/app/src/main/java/edu/gatech/gtmaps/SearchObject.java
@@ -3,6 +3,7 @@ package edu.gatech.gtmaps;
 import java.util.LinkedList;
 import edu.gatech.gtmaps.models.Building;
 import edu.gatech.gtmaps.models.Room;
+import edu.gatech.gtmaps.models.Hall;
 
 /**
  * Singleton search object for the application
@@ -21,5 +22,14 @@ public class SearchObject {
     public LinkedList<Room> find(String roomNumber, Building b) {
         LinkedList<Room> ret = new LinkedList();
         return ret;
+    }
+
+    private class Node {
+        private Hall h;
+        private Hall p;
+        private Node(Hall thisHallway, Hall previousHallway) {
+            h = thisHallway;
+            p = previousHallway;
+        }
     }
 }
