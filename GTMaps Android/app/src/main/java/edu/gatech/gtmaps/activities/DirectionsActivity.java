d30e21ea8b4c160ffd82ea58f240f74dd274b3d5
Brooks, Keanu Lee <Keanu.B.Lee@gatech.edu>
2016-11-16 13:26:23 -0500

    Some UI changes
    Added three dot settings icon to UI

--- GTMaps Android/app/src/main/java/edu/gatech/gtmaps/activities/DirectionsActivity.java
+++ GTMaps Android/app/src/main/java/edu/gatech/gtmaps/activities/DirectionsActivity.java
@@ -3,9 +3,14 @@ package edu.gatech.gtmaps.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Arrays;
 
 import edu.gatech.gtmaps.R;
 
@@ -30,6 +35,33 @@ public class DirectionsActivity extends AppCompatActivity {
 
         ImageView iv = (ImageView)findViewById(R.id.ivfloorplan);
         iv.setImageResource(R.drawable.ccbfloor);
+    }
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.actions, popup.getMenu());
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent1;
+
+                if (item.getItemId() == R.id.go_to_building) {
+                    intent1 = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent1);
+                    return true;
+                } else if(item.getItemId() == R.id.choose_room) {
+                    intent1 = new Intent(getApplicationContext(), RoomChooseActivity.class);
+                    String message = getIntent().getStringExtra("building");
+                    intent1.putExtra("building", message);
+                    startActivity(intent1);
+                    return true;
+                }
+                return false;
+            }
+        });
+        popup.show();
     }
 
     public void home(View view) {
--- GTMaps Android/app/src/main/java/edu/gatech/gtmaps/activities/MainActivity.java
+++ GTMaps Android/app/src/main/java/edu/gatech/gtmaps/activities/MainActivity.java
@@ -5,6 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
@@ -62,11 +66,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void home(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.actions, popup.getMenu());
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
 
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent1;
+
+                if (item.getItemId() == R.id.go_to_building) {
+                    intent1 = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent1);
+                    return true;
+                } else if(item.getItemId() == R.id.choose_room) {
+                    intent1 = new Intent(getApplicationContext(), RoomChooseActivity.class);
+                    String message = getIntent().getStringExtra("building");
+                    intent1.putExtra("building", message);
+                    startActivity(intent1);
+                    return true;
+                }
+                return false;
+            }
+        });
+        popup.show();
+    }
     public void hideKeyboard(View view) {
         InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
