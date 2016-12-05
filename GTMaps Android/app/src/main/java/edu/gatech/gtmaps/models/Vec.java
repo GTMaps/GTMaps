bd6e72b414f9069d2150364a870465cd4f60c041
Christine Shi <yshi84@gatech.edu>
2016-12-05 13:35:22 -0500

    Added Javadoc

--- GTMaps Android/app/src/main/java/edu/gatech/gtmaps/models/Vec.java
+++ GTMaps Android/app/src/main/java/edu/gatech/gtmaps/models/Vec.java
@@ -6,84 +6,33 @@ package edu.gatech.gtmaps.models;
 
 public class Vec {
     float x, y;
+
+    //constructor
     Vec(float px, float py) {
         x = px;
         y = py;
     };
-public Vec(Point P, Point Q) {
+
+    //constructor
+    public Vec(Point P, Point Q) {
         x = Q.x-P.x;
         y = Q.y-P.y;
     }
-    public void printVec() {
-        System.out.println("vec x: "+x+"vec y: "+y);
-    }
-    // MODIFY
-    Vec setTo(float px, float py) {
-        x = px;
-        y = py;
-        return this;
-    }
-    Vec setTo(Vec V) {
-        x = V.x;
-        y = V.y;
-        return this;
-    }
-    Vec zero() {
-        x=0;
-        y=0;
-        return this;
-    }
-    Vec scaleBy(float u, float v) {
-        x*=u;
-        y*=v;
-        return this;
-    }
-    Vec scaleBy(float f) {
-        x*=f;
-        y*=f;
-        return this;
-    }
-    Vec reverse() {
-        x=-x;
-        y=-y;
-        return this;
-    }
-    Vec divideBy(float f) {
-        x/=f;
-        y/=f;
-        return this;
-    }
-    Vec normalize() {
-        double n=Math.sqrt(x*x+y*y);
-        if (n>0.000001) {
-            x/=n;
-            y/=n;
-        }
-        return this;
-    }
-    Vec add(float u, float v) {
-        x += u;
-        y += v;
-        return this;
-    }
-    Vec add(Vec V) {
-        x += V.x;
-        y += V.y;
-        return this;
-    }
-    Vec add(float s, Vec V) {
-        x += s*V.x;
-        y += s*V.y;
-        return this;
-    }
 
-    //left this be the current forward direction
-    //If this method returns true, the turn is a left turn.
-    //Else it is a right turn
+  
+    /**
+     * left this be the current forward direction. If this method returns true, the turn is a left turn.Else it is a right turn
+     * @param prevForwardDirection previous forard direction
+     * @return Boolean if the turn is a left turn.
+     */  
     public boolean isLeftTurn(Vec prevForwardDirection) {
         return det(prevForwardDirection,this) > 0;
     }
-    //a here is in radian, rotate in clockwise
+    /**
+     * rotate in clockwise
+     * @param a radian
+     * @return Vec vector after rotation
+     */  
     Vec rotateBy(float a) {
         float xx=x, yy=y;
         x= (float) (xx*Math.cos(a)-yy*Math.sin(a));
@@ -91,62 +40,32 @@ public Vec(Point P, Point Q) {
         return this;
     }
 
+     /**
+     * dot(this,V): U*V (dot product U*V)
+     * @param V R vectors to be calcuated
+     * @return float dot product result
+     */    
     float dot(Vec U, Vec V) {
         return U.x*V.x+U.y*V.y;
-    }                                                     // dot(this,V): U*V (dot product U*V)
+    } 
+
+     /**
+     * dot product for no normal of U and V    
+     * @param V R vectors to be calcuated
+     * @return float det product result
+     */                                              
     float det(Vec U, Vec V) {
         return dot(R(U), V);
     }
 
+    /**
+     * V turned 90 degrees (clockwise)
+     * @param V vector to be rotated
+     * @return Vec vector after rotation
+     */
     Vec R(Vec V) {
         return new Vec(-V.y, V.x);
-    }                                                            // V turned 90 degrees (clockwise)
+    }                                                            
 
-    Vec left() {
-        float m=x;
-        x=-y;
-        y=m;
-        return this;
-    }
 
-//    // OUTPUT VEC
-//    Vec clone() {
-//        return(new Vec(x, y));
-//    };
-
-    // OUTPUT TEST MEASURE
-    double norm() {
-        return(Math.sqrt(x*x+y*y));
-    }
-    boolean isNull() {
-        return((Math.abs(x)+Math.abs(y)<0.000001));
-    }
-    double angle() {
-        return(Math.atan2(y, x));
-    }
-
-    // DRAW, PRINT
-    void write() {
-        System.out.println("<"+x+","+y+">");
-    }
-//    void showAt (pt P) {
-//        line(P.x, P.y, P.x+x, P.y+y);
-//    };
-//    void showArrowAt (pt P)
-//    {
-//        line(P.x, P.y, P.x+x, P.y+y);
-//        float n=min(this.norm()/10., height/50.);
-//        pt Q=P(P, this);
-//        Vec U = S(-n, U(this));
-//        Vec W = S(.3, R(U));
-//        beginShape();
-//        Q.add(U).add(W).v();
-//        Q.v();
-//        Q.add(U).add(M(W)).v();
-//        endShape(CLOSE);
-//    }
-//
-//    void label(String s, pt P) {
-//        P(P).add(0.5, this).add(3, R(U(this))).label(s);
-//    }
 }
